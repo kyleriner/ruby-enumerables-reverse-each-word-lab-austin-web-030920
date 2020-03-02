dc2b8b7f51ec937351f3babdd688f7c43da75d46
Kyle Riner <rinerkyle97@gmail.com>
2020-03-02 03:40:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   words = sentence.split(" ")
   i = 0
   while i < words.length do
-    words[i].reverse()
+    words[i].reverse!
     i += 1
   end
   return words.join(" ")
